dcfb7b7fbfce2dc0497ab6cb6b71174ec220d0f0
Gabriel Mackie <gm975@york.ac.uk>
2021-09-01 21:04:48 +0100

    The set of blocks can now repeat, with a rest block in-between each set. A slight issue is that when the second set plays each video starts on its last frame for a second before beginning again

--- Davids_Vid_Exp.py
+++ Davids_Vid_Exp.py
@@ -5,16 +5,18 @@ from psychopy import visual, core, gui, event
 import os
 from datetime import datetime
 
-# Get the working directory, where our file is
-cwd = os.getcwd()
-
 # Set the name of our spreadsheet with the stimuli in
 stimuli = 'Stimuli_List.xlsx'
 
 # Set escape key
 escape_key = 'escape'
 
-categories = ['Talking', 'Gaze', 'Expressions', 'Couple Talking', 'Head Turn']
+# Set the number of times the whole 5 blocks should repeat, with a rest block between each set
+reps = 2
+
+
+# Get the working directory, where our file is
+cwd = os.getcwd()
 
 # Get information from the participant and set up our output file
 # Create variables to hold the participant information
@@ -95,98 +97,112 @@ for row in range(rows):
         if stim_type == cat:
             stimuli[stim_type].append(stim)
 
-# Shuffle the stimuli within each block
-for group in stimuli:
-    shuffle(stimuli[group])
-    
-# Take the category names so we can call each one
-keys = list(stimuli.keys())
-
-# Shuffle the order of the blocks
-shuffle(keys)
-
-
 # EXPERIMENT STARTS HERE
 # Set the clock (Not sure what it does, don't delete)
 globalClock = core.Clock()
 
-# Have an introductory rest block
+for rep in range(reps):
+    # Shuffle the stimuli within each block
+    for group in stimuli:
+        shuffle(stimuli[group])
+        
+    # Take the category names so we can call each one
+    keys = list(stimuli.keys())
+    
+    # Shuffle the order of the blocks
+    shuffle(keys)
+    
+    # Have an introductory rest block
+    timing = win.flip()
+    # Write out the block information
+    f.write(f'rest,NA,{timing},NA\n')
+    f.flush()
+    core.wait(4)
+    
+    # Run each cateogry of stimulus at a time
+    for key in keys:
+        # Extract the block from our dictionary
+        block = stimuli[key]
+        
+        # Calculate the number of trials in a block
+        length = len(block)
+        # Pick a random trial to play the red dot
+        red_trial = choice(range(length)) + 1
+        trial = 1
+        
+        # Now play each video within that block
+        # You need to be careful how much you put in here, because it can slow the code down
+        for stim in block:
+            # Assign the stimuli to our mov object
+            mov = stim
+            
+            # Set the trial to be a red-dot or normal trial
+            # This is seperate from the while loop in case we want a user response to 
+            # end the dot. We'd do that by setting it to false
+            if trial == red_trial:
+                circ = True
+            else:
+                circ = False
+            
+            # Clear the events so any keys pressed previously won't count
+            event.clearEvents()
+            
+            # Extract the time that the video starts
+            timing = core.getTime()
+            # This runs the actual video
+            while mov.status != visual.FINISHED:
+                mov.draw()
+                # Drawing a circle if needed
+                if circ:
+                    circle.draw()
+                win.flip()
+                
+            mov.seek(0)
+            mov.status = visual.NOT_STARTED
+            
+            # Get the keys that the participant has pressed
+            keys = event.getKeys()
+            
+            # The length of the keys is the number of keys pressed
+            key_presses = len(keys)
+            
+            # Set the response to the default of NA
+            response = 'NA'
+            
+            if key_presses > 0:
+                # If the key pressed was the escape key, print that to the output and quit the experiment
+                if keys[0] == escape_key:
+                    f.write('User requested to quit: ending experiment')
+                    f.close()
+                    win.close()
+                    core.quit()
+                
+                # If it wasn't the esc key, and the red dot trial happened, it means they responded
+                elif circ:
+                    response = True
+            
+            # If no keys were pressed and it was the red dot trial, then they failed to respond
+            else:
+                if circ:
+                    response = False
+            
+            # Extract the name out of the mov stimulus
+            name = stim.name
+            # Write this trial's information to the output file
+            f.write(f'{key},{name},{timing},{response}\n')
+            f.flush()
+            
+            # increase the trial num by one
+            trial += 1
+
+# Have an final rest block
 timing = win.flip()
 # Write out the block information
 f.write(f'rest,NA,{timing},NA\n')
 f.flush()
 core.wait(4)
+# TODO Does he want a final time file here?
 
-# Run each cateogry of stimulus at a time
-for cat, block in stimuli.items():
-    # Calculate the number of trials in a block
-    length = len(block)
-    # Pick a random trial to play the red dot
-    red_trial = choice(range(length)) + 1
-    trial = 1
-    
-    # Now play each video within that block
-    # You need to be careful how much you put in here, because it can slow the code down
-    for stim in block:
-        # Assign the stimuli to our mov object
-        mov = stim
-        
-        # Set the trial to be a red-dot or normal trial
-        # This is seperate from the while loop in case we want a user response to 
-        # end the dot. We'd do that by setting it to false
-        if trial == red_trial:
-            circ = True
-        else:
-            circ = False
-        
-        # Clear the events so any keys pressed previously won't count
-        event.clearEvents()
-        
-        # Extract the time that the video starts
-        timing = core.getTime()
-        # This runs the actual video
-        while mov.status != visual.FINISHED:
-            mov.draw()
-            # Drawing a circle if needed
-            if circ:
-                circle.draw()
-            win.flip()
-        
-        # Get the keys that the participant has pressed
-        keys = event.getKeys()
-        
-        # The length of the keys is the number of keys pressed
-        key_presses = len(keys)
-        
-        # Set the response to the default of NA
-        response = 'NA'
-        
-        if key_presses > 0:
-            # If the key pressed was the escape key, print that to the output and quit the experiment
-            if keys[0] == escape_key:
-                f.write('User requested to quit: ending experiment')
-                f.close()
-                win.close()
-                core.quit()
-            
-            # If it wasn't the esc key, and the red dot trial happened, it means they responded
-            elif circ:
-                response = True
-        
-        # If no keys were pressed and it was the red dot trial, then they failed to respond
-        else:
-            if circ:
-                response = False
-        
-        # Extract the name out of the mov stimulus
-        name = stim.name
-        # Write this trial's information to the output file
-        f.write(f'{cat},{name},{timing},{response}\n')
-        f.flush()
-        
-        # increase the trial num by one
-        trial += 1
-    
 # Close the experiment
 f.close()
 win.close()
