6a6005693e0a0e99a2268577ed43490d1e7876aa
Gabriel Mackie <gm975@york.ac.uk>
2021-09-16 12:20:03 +0100

    Added the start of the timings calculator, essentially just an outline at the moment

---
+++ Timings_Calculator.R
@@ -0,0 +1,11 @@
+# Load in data
+
+# Calculate the timings
+
+# Add the timings back to the df
+
+# Print it
+
+# Write it back to the file?
+
+https://www.geeksforgeeks.org/append-row-to-csv-using-r/
