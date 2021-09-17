1fb1a0299bf0c7cc29218b918bc911fe279cea59
Gabriel Mackie <gm975@york.ac.uk>
2021-09-17 13:19:00 +0100

    Added a timings calculator that works out the amount of time lapsed from each timepoint to the next

--- Timings_Calculator.R
+++ Timings_Calculator.R
@@ -1,11 +1,20 @@
+library(tidyverse)
+
+file = "20210917_1210_P1.csv"
+
 # Load in data
+data <- read_csv(file)
 
 # Calculate the timings
-
-# Add the timings back to the df
-
+data <- data %>%
+  mutate(
+    adjusted_timing = lag(timing, default = 0) # Add a variable with the timings shifted forwards
+  ) %>%
+  mutate(
+    difference = timing - adjusted_timing # Calculate the difference to show time elapsed since the last timestamp
+  )
 # Print it
+data
 
-# Write it back to the file?
-
-https://www.geeksforgeeks.org/append-row-to-csv-using-r/
+# Write it back to the file
+write_csv(data, file)
